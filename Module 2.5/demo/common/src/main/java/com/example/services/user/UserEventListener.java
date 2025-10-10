ec6fb9d6aa42f39262fb39fc9d119f6bc4106369
LisenokArtist <lisenokartist@gmail.com>
2025-10-10 22:56:20 +0300

    Правка прослушивателя и сервиса рассылок

--- Module 2.5/demo/common/src/main/java/com/example/services/user/UserEventListener.java
+++ Module 2.5/demo/common/src/main/java/com/example/services/user/UserEventListener.java
@@ -1,5 +1,10 @@
 package com.example.services.user;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -10,13 +15,37 @@ import lombok.Getter;
 @Component
 @Getter
 public class UserEventListener{
+    private final List<ActionListener> listeners = new ArrayList<>();
+    
     private UserEvent receivedEvent;
 
+    /**
+     * Метод для добавления слушателя
+     * (подписка на события)
+     * @param listener Слушатель
+     */
+    public void addActionListener(ActionListener listener) {
+        listeners.add(listener);
+    }
+
+    /**
+     * Вызывается каждый раз, когда
+     * kafka присылает уведомления
+     * @param event Событие
+     */
     @KafkaListener(
         topics="${spring.kafka.user.topic}", 
         groupId="${spring.kafka.user.group}", 
         containerFactory="userListenerContainerFactory")
     public void listenUserEvents(UserEvent event){
+        ActionEvent actionEvent = new ActionEvent(event, ActionEvent.ACTION_PERFORMED, UserEventListener.Events.onUserEvent.toString());
+        listeners.stream().forEach(x -> x.actionPerformed(actionEvent));
+        
         this.receivedEvent = event;
+    }
+
+    /** Список событий генерируемые этим классом */
+    public static enum Events {
+        onUserEvent
     }
 }
--- Module 2.5/demo/user-notification-service/src/main/java/com/example/services/EmailService.java
+++ Module 2.5/demo/user-notification-service/src/main/java/com/example/services/EmailService.java
@@ -7,17 +7,55 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import com.example.datamodels.models.mail.MailShort;
+import com.example.datamodels.models.user.UserEvent;
+import com.example.services.user.UserEventListener;
 
 @Service
 public class EmailService {
     @Autowired
     private final JavaMailSender mailSender;
+    @Autowired
+    private final UserEventListener listener;
 
-    public EmailService(JavaMailSender mailSender){
+
+    public EmailService(JavaMailSender mailSender, UserEventListener listener){
         this.mailSender = mailSender;
+        this.listener = listener;
+
+        registerEventHandlers();
     }
 
-    public void sendAccountCreatedEmail(String to) {
+    
+    /**
+     * Подписываемся на события, которые
+     * генерирует UserEventListener
+     */
+    private void registerEventHandlers(){
+        listener.addActionListener(e -> {
+            UserEventListener.Events event = UserEventListener.Events.valueOf(e.getActionCommand());
+            switch(event){
+                case onUserEvent -> onUserEvent((UserEvent)e.getSource());
+            }
+        });
+    }
+
+    /**
+     * Обработчик события onUserEvent
+     * @param userEvent событие
+     */
+    private void onUserEvent(UserEvent userEvent) {
+        switch (userEvent.getEventType()) {
+            case CREATE -> sendAccountCreatedEmail(userEvent.getUserEmail());
+            case DELETE -> sendAccountDeletedEmail(userEvent.getUserEmail());
+        }
+    }
+
+    /**
+     * Отправляет системное сообщение
+     * на почту о создании
+     * @param to email получателя
+     */
+    private void sendAccountCreatedEmail(String to) {
         sendEmail(
             null, 
             to, 
@@ -25,7 +63,12 @@ public class EmailService {
             "Здравствуйте! Ваш аккаунт на сайте был успешно создан.");
     }
 
-    public void sendAccountDeletedEmail(String to) {
+    /**
+     * Отправляет системное сообщение
+     * на почту о удалении
+     * @param to email получателя
+     */
+    private void sendAccountDeletedEmail(String to) {
         sendEmail(
             null, 
             to, 
@@ -33,6 +76,10 @@ public class EmailService {
             "Здравствуйте! Ваш аккаунт был удалён.");
     }
 
+    /**
+     * Отправляет сообщение на email
+     * @param mail модель сообщения
+     */
     public void sendEmail(MailShort mail){
         this.sendEmail(
             mail.getFrom(), 
@@ -41,6 +88,13 @@ public class EmailService {
             mail.getText());
     }
 
+    /**
+     * Отправляет сообщение на email
+     * @param from от кого
+     * @param to куда
+     * @param subject тема
+     * @param text сообщение
+     */
     public void sendEmail(@Nullable String from, String to, String subject, String text){
         SimpleMailMessage message = new SimpleMailMessage();
         if (from != null) message.setFrom(from);
