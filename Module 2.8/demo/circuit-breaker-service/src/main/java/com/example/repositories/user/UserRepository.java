38d643b8f612cb1fb51c10376d2add7903bcdfc2
LisenokArtist <lisenokartist@gmail.com>
2025-10-24 15:55:20 +0300

    Удален лишний circuitbreaker сервис

--- Module 2.8/demo/circuit-breaker-service/src/main/java/com/example/repositories/user/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.repositories.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.datamodels.entities.user.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
